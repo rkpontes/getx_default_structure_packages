0ccea19a418c792fb2642761b301fc534526e3b1
Raphael MacOS <raphael@betacoding.com.br>
2020-08-25 17:58:36 -0300

    third  commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_default_structure_packages/app/routes/app_pages.dart';
+import 'package:getx_default_structure_packages/app/routes/app_routes.dart';
 import 'package:getx_default_structure_packages/app/translations/app_translations.dart';
 import 'package:getx_default_structure_packages/app/ui/theme/app_theme.dart';
 
@@ -10,7 +11,7 @@ void main() async{
       title: 'App Name',
       debugShowCheckedModeBanner: false,
       getPages: AppPages.routes,
-      initialRoute: '',
+      initialRoute: Routes.HOME,
       theme: appTheme,
       darkTheme: appThemeDark,
       themeMode: ThemeMode.system,
