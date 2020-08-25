30223840a7095defbc4b6961102bed264119eb6b
Raphael MacOS <raphael@betacoding.com.br>
2020-08-25 17:55:17 -0300

    second commit

--- lib/app/translations/en_US/en_us_translations.dart
+++ lib/app/translations/en_US/en_us_translations.dart
@@ -1,7 +1,3 @@
 final Map<String, String> enUs = {
   //'CHAVE' : 'VALOR-TRADUZIDO',
-  'TITLE_HOME_PAGE' : 'Home Page',
-  'TEXT_BT_THEME_LIGHT' : 'Theme: Light',
-  'TEXT_BT_THEME_DARK' : 'Theme: Dark',
-  'TEXT_BT_LOGOUT' : 'Logout',
 };
--- lib/app/translations/pt_BR/pt_br_translation.dart
+++ lib/app/translations/pt_BR/pt_br_translation.dart
@@ -1,8 +1,3 @@
 final Map<String, String> ptBr = {
   //'CHAVE' : 'VALOR-TRADUZIDO',
-  'TITLE_HOME_PAGE' : 'Página Inicial',
-  'TEXT_BT_THEME_LIGHT' : 'Tema: Claro',
-  'TEXT_BT_THEME_DARK' : 'Tema: Escuro',
-  'TEXT_BT_LOGOUT' : 'Sair',
-  
 };
